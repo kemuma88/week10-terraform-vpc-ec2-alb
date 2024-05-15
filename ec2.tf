bcddb614ca7d90af58a6b9fb6fe1f1ed700d806d
Kemuma88 <ongwaemarion@gmail.com>
2024-05-14 20:36:34 -0500

    modified

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "server1" {
     ami = "ami-02d7fd1c2af6eead0"
     instance_type = var.INSTANCE_TYPE
     vpc_security_group_ids = [ aws_security_group.sg1.id ]
-    availability_zone = "${var.REGION}"a
+    availability_zone = "${var.REGION}a"
     subnet_id = aws_subnet.private1.id
   user_data = file("code.sh")
   tags={
